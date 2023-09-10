b39e131fcd25f000848d035952ff73182ae4cb15
Lwandile Mfundisi <Lwandile.Mfundisi@media24.com>
2023-09-10 21:06:58 +0200

    Add project files.

---
+++ XFrame.Aggregates.Commands.Extensions/DomainContainerCommandExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using XFrame.DomainContainers;
+
+namespace XFrame.Aggregates.Commands.Extensions
+{
+    public static class DomainContainerCommandExtensions
+    {
+        public static IDomainContainer AddCommands(
+            this IDomainContainer domainContainer,
+            params Type[] commandTypes)
+        {
+            return domainContainer.AddCommands(commandTypes);
+        }
+
+        public static IDomainContainer AddCommands(
+            this IDomainContainer domainContainer,
+            Assembly fromAssembly,
+            Predicate<Type> predicate)
+        {
+            predicate = predicate ?? (t => true);
+            var commandTypes = fromAssembly
+                .GetTypes()
+                .Where(t => !t.GetTypeInfo().IsAbstract && typeof(ICommand).GetTypeInfo().IsAssignableFrom(t))
+                .Where(t => predicate(t));
+            return domainContainer.AddTypes(commandTypes);
+        }
+    }
+}
---
+++ XFrame.Aggregates.Commands.Extensions/DomainContainerCommandHandlerExtensions.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using XFrame.Common.Extensions;
+using XFrame.DomainContainers;
+
+namespace XFrame.Aggregates.Commands.Extensions
+{
+    public static class DomainContainerCommandHandlerExtensions
+    {
+        public static IDomainContainer AddCommandHandlers(
+            this IDomainContainer domainContainer,
+            Assembly fromAssembly,
+            Predicate<Type> predicate = null)
+        {
+            predicate = predicate ?? (t => true);
+            var commandHandlerTypes = fromAssembly
+                .GetTypes()
+                .Where(t => t.GetTypeInfo().GetInterfaces().Any(IsCommandHandlerInterface))
+                .Where(t => !t.HasConstructorParameterOfType(IsCommandHandlerInterface))
+                .Where(t => predicate(t));
+            return domainContainer.AddCommandHandlers(commandHandlerTypes);
+        }
+
+        public static IDomainContainer AddCommandHandlers(
+            this IDomainContainer domainContainer,
+            params Type[] commandHandlerTypes)
+        {
+            return domainContainer.AddCommandHandlers((IEnumerable<Type>)commandHandlerTypes);
+        }
+
+        public static IDomainContainer AddCommandHandlers(
+            this IDomainContainer domainContainer,
+            IEnumerable<Type> commandHandlerTypes)
+        {
+            foreach (var commandHandlerType in commandHandlerTypes)
+            {
+                var t = commandHandlerType;
+                if (t.GetTypeInfo().IsAbstract) continue;
+                var handlesCommandTypes = t
+                    .GetTypeInfo()
+                    .GetInterfaces()
+                    .Where(IsCommandHandlerInterface)
+                    .ToList();
+                if (!handlesCommandTypes.Any())
+                {
+                    throw new ArgumentException($"Type '{commandHandlerType.PrettyPrint()}' does not implement '{typeof(ICommandHandler<,,,>).PrettyPrint()}'");
+                }
+
+                foreach (var handlesCommandType in handlesCommandTypes)
+                {
+                    domainContainer.ServiceCollection.AddTransient(handlesCommandType, t);
+                }
+            }
+
+            return domainContainer;
+        }
+
+        private static bool IsCommandHandlerInterface(this Type type)
+        {
+            return type.GetTypeInfo().IsGenericType && type.GetGenericTypeDefinition() == typeof(ICommandHandler<,,,>);
+        }
+    }
+}
